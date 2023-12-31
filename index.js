939bde69bf87d8bc7dc3a1300ce9d9e8161022b8
Treasure-Porth <112691973+Treasure-Porth@users.noreply.github.com>
2023-12-31 11:56:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Name = document.getElementById("name")
 const Pass = document.getElementById("password")
 const sub = document.getElementById("submit")
 const incorrect = document.getElementById("incorrect")
+let i = false 
 
 sub.addEventListener("click", function(){
     checker()
@@ -10,21 +11,30 @@ sub.addEventListener("click", function(){
 function checker() {
     if (Name.value == "ayesha" && Pass.value == "chompa") {
         window.location.href = "./samreen.html"
+        i = true
     }
-    else if (Name.value == "fatema" && Pass.value == "naruto") {
+    if (Name.value == "fatema" && Pass.value == "naruto") {
         window.location.href = "./fatema.html"
+        i = true
     }
-    else if  (Name.value == "aunton" && Pass.value == "rajshahi") {
+    if  (Name.value == "aunton" && Pass.value == "rajshahi") {
         window.location.href = "./aunton.html"
+        i = true
     }
-    else if (Name.value == "alif" && Pass.value == "murikha") {
+    if (Name.value == "alif" && Pass.value == "murikha") {
         window.location.href = "./alif.html"
+        i = true
     }
-    else if (Name.value == "samanta" && Pass.value == "egg") {
+    if (Name.value == "samanta" && Pass.value == "egg") {
         window.location.href = "./samanta.html"
+        i = true
     }
-    else{
-        let text = "invalid name or password. i didn't create any account on that name or password. try again"
-        incorrect.innerHTML = text;
-    }
+    dblcheck()
+
 }
+ function dblcheck() {
+     if ( i == false ) {
+         let msg = "invalid username or password.i didn't make any acc with that"
+         incorrect.innerhtml = msg
+     }
+ }
